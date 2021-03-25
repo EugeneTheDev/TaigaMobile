4ba3773c151edeeca6739200d669da91f62f6bf7
Eugene Seager <eugene.seager@gmail.com>
2021-03-26 01:36:19 +0300

    Bump app version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "io.eugenethedev.taigamobile"
         minSdkVersion(22)
         targetSdkVersion(30)
-        versionCode(1)
-        versionName = "0.1-alpha"
+        versionCode(2)
+        versionName = "0.2-alpha"
         project.base.archivesBaseName = "TaigaMobile-$versionName"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
