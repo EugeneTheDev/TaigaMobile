9f80ca73a1df2c118e8dfe6b83fea53d90e1ca28
Eugene Seager <eugene.seager@gmail.com>
2021-03-26 01:47:29 +0300

    Bump libs versions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -85,11 +85,11 @@ dependencies {
     implementation("androidx.compose.runtime:runtime-livedata:$composeVersion")
     implementation("androidx.compose.animation:animation:$composeVersion")
     // compose activity
-    implementation("androidx.activity:activity-compose:1.3.0-alpha04")
+    implementation("androidx.activity:activity-compose:1.3.0-alpha05")
     // view model support
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:1.0.0-alpha03")
     // compose constraint layout
-    implementation("androidx.constraintlayout:constraintlayout-compose:1.0.0-alpha04")
+    implementation("androidx.constraintlayout:constraintlayout-compose:1.0.0-alpha05")
     // glide with compose
     implementation("dev.chrisbanes.accompanist:accompanist-glide:0.6.1")
 
