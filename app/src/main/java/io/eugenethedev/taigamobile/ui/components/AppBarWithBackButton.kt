73b2f38e5ca4884a79b6e2585e7c5a00e597bb60
Eugene Seager <eugene.seager@gmail.com>
2021-03-26 13:23:46 +0300

    Indicate click on back button in toolbar

--- app/src/main/java/io/eugenethedev/taigamobile/ui/components/AppBarWithBackButton.kt
+++ app/src/main/java/io/eugenethedev/taigamobile/ui/components/AppBarWithBackButton.kt
@@ -1,16 +1,11 @@
 package io.eugenethedev.taigamobile.ui.components
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.interaction.MutableInteractionSource
-import androidx.compose.foundation.interaction.collectIsPressedAsState
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.res.painterResource
@@ -24,28 +19,14 @@ fun AppBarWithBackButton(
 ) = TopAppBar(
     title = title,
     navigationIcon = {
-        val interactionSource = remember { MutableInteractionSource() }
-        val pressedState by interactionSource.collectIsPressedAsState()
-
-        Image(
-            painter = painterResource(R.drawable.ic_arrow_back),
-            contentDescription = null,
-            colorFilter = ColorFilter.tint(
-                if (pressedState) {
-                    MaterialTheme.colors.primaryVariant
-                } else {
-                    MaterialTheme.colors.primary
-                }
-            ),
-            modifier = Modifier
-                .size(36.dp)
-                .padding(start = 8.dp)
-                .clickable(
-                    interactionSource = interactionSource,
-                    indication = null,
-                    onClick = navigateBack
-                )
-        )
+        IconButton(onClick = navigateBack) {
+            Image(
+                painter = painterResource(R.drawable.ic_arrow_back),
+                contentDescription = null,
+                colorFilter = ColorFilter.tint(MaterialTheme.colors.primary),
+                modifier = Modifier.size(28.dp)
+            )
+        }
     },
     backgroundColor = MaterialTheme.colors.surface,
     elevation = 0.dp
