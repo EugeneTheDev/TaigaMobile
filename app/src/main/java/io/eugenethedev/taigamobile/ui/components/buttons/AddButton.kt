4c387283625f9f8ec0ec39d11459315d7085950a
Eugene Seager <eugene.seager@gmail.com>
2021-07-23 22:44:56 +0300

    Add slight background on AddButton

--- app/src/main/java/io/eugenethedev/taigamobile/ui/components/buttons/AddButton.kt
+++ app/src/main/java/io/eugenethedev/taigamobile/ui/components/buttons/AddButton.kt
@@ -2,15 +2,13 @@ package io.eugenethedev.taigamobile.ui.components.buttons
 
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
-import androidx.compose.material.Icon
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
-import androidx.compose.material.TextButton
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import io.eugenethedev.taigamobile.R
+import io.eugenethedev.taigamobile.ui.theme.taigaLightGray
 
 /**
  * Text button with plus icon on the left
@@ -21,18 +19,18 @@ fun AddButton(
     onClick: () -> Unit
 ) = TextButton(
     onClick = onClick,
+    colors = ButtonDefaults.textButtonColors(
+        backgroundColor = taigaLightGray
+    ),
     contentPadding = PaddingValues(horizontal = 8.dp, vertical = 4.dp)
 ) {
     Row(verticalAlignment = Alignment.CenterVertically) {
         Icon(
             painter = painterResource(R.drawable.ic_add),
-            contentDescription = null,
-            tint = MaterialTheme.colors.primary
+            contentDescription = null
         )
 
-        Text(
-            text = text,
-            color = MaterialTheme.colors.primary
-        )
+        Text(text)
     }
 }
+
