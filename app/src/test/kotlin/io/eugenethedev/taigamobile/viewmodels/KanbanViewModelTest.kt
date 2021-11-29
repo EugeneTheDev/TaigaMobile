ad6b785830430840231fbacd6bc4b456107ba68c
MIshkaV <mavorozhtsov_1@edu.hse.ru>
2021-11-29 18:24:40 +0300

    created kanbanViewModelTest

--- app/src/test/kotlin/io/eugenethedev/taigamobile/viewmodels/KanbanViewModelTest.kt
+++ app/src/test/kotlin/io/eugenethedev/taigamobile/viewmodels/KanbanViewModelTest.kt
@@ -1,18 +1,103 @@
 package io.eugenethedev.taigamobile.viewmodels
 
-import io.eugenethedev.taigamobile.domain.entities.Swimlane
+import androidx.lifecycle.viewmodel.compose.viewModel
+import io.eugenethedev.taigamobile.domain.entities.*
+import io.eugenethedev.taigamobile.testdata.TestData
 import io.eugenethedev.taigamobile.ui.screens.kanban.KanbanViewModel
+import io.eugenethedev.taigamobile.ui.utils.SuccessResult
+import io.eugenethedev.taigamobile.viewmodels.utils.toTeamMember
+import io.mockk.coEvery
 import kotlinx.coroutines.runBlocking
 import kotlin.test.BeforeTest
 import kotlin.test.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertIs
 
 class KanbanViewModelTest : BaseViewModelTest() {
-    private lateinit var viewModel : KanbanViewModel
+    private lateinit var viewModel: KanbanViewModel
 
     @BeforeTest
     fun setup() {
         viewModel = KanbanViewModel(mockAppComponent)
+    }
+
+    private fun <T> checkListsSize(expectedList: List<T>?, actualList: List<T>?) {
+        assertEquals(
+            expected = expectedList?.size,
+            actual = actualList?.size
+        )
+    }
+
+    @Test
+    fun `test on open`(): Unit = runBlocking {
+        val testStatuses =
+            mockTaskRepository.getStatuses(CommonTaskType.UserStory).sortedBy { it.name }
+        val testTeam = TestData.users.sortedBy { it.username }.mapIndexed { index, user ->
+            user.toTeamMember(
+                id = index.toLong(),
+                name = "testName${index}",
+                role = "testRole${index}",
+                totalPower = index
+            )
+        }
+        val testSwimlanes = listOf(Swimlane(1, "test swimlane", 1))
+
+        coEvery { mockTaskRepository.getStatuses(any()) } returns testStatuses
+        coEvery { mockUsersRepository.getTeam() } returns testTeam
+        coEvery { mockTaskRepository.getSwimlanes() } returns testSwimlanes
+
+        viewModel.onOpen()
+        assertIs<SuccessResult<List<Status>>>(viewModel.statuses.value)
+        assertIs<SuccessResult<List<User>>>(viewModel.team.value)
+        assertIs<SuccessResult<List<CommonTaskExtended>>>(viewModel.stories.value)
+        assertIs<SuccessResult<List<Swimlane?>>>(viewModel.swimlanes.value)
+
+        checkListsSize(testStatuses, viewModel.statuses.value.data)
+        viewModel.statuses.value.data?.let { statuses ->
+            statuses.sortedBy { it.name }.forEachIndexed { index, status ->
+                assertEquals(
+                    expected = testStatuses[index].name,
+                    actual = status.name
+                )
+                assertEquals(
+                    expected = testStatuses[index].color,
+                    actual = status.color
+                )
+            }
+        }
+
+        checkListsSize(testTeam, viewModel.team.value.data)
+        viewModel.team.value.data?.let { users ->
+            users.sortedBy { it.username }.forEachIndexed { index, user ->
+                assertEquals(
+                    expected = testTeam[index].id,
+                    actual = user.id
+                )
+                assertEquals(
+                    expected = testTeam[index].username,
+                    actual = user.username
+                )
+            }
+        }
+
+        val testSwimlanesWithNull = (listOf(null) + testSwimlanes).sortedBy { it?.id }
+        checkListsSize(testSwimlanesWithNull, viewModel.swimlanes.value.data)
+        viewModel.swimlanes.value.data?.let { swimlanes ->
+            swimlanes.sortedBy { it?.id }.forEachIndexed { index, swimlane ->
+                assertEquals(
+                    expected = testSwimlanesWithNull[index]?.id,
+                    actual = swimlane?.id
+                )
+                assertEquals(
+                    expected = testSwimlanesWithNull[index]?.name,
+                    actual = swimlane?.name
+                )
+                assertEquals(
+                    expected = testSwimlanesWithNull[index]?.order,
+                    actual = swimlane?.order
+                )
+            }
+        }
     }
 
     @Test
@@ -20,9 +105,15 @@ class KanbanViewModelTest : BaseViewModelTest() {
         val swimlane = Swimlane(1, "test name", 1)
 
         viewModel.selectSwimlane(swimlane)
-        assertEquals(viewModel.selectedSwimlane.value, swimlane)
+        assertEquals(
+            expected = swimlane,
+            actual = viewModel.selectedSwimlane.value
+        )
 
         viewModel.selectSwimlane(null)
-        assertEquals(viewModel.selectedSwimlane.value, null)
+        assertEquals(
+            expected = null,
+            actual = viewModel.selectedSwimlane.value
+        )
     }
 }
--- app/src/test/kotlin/io/eugenethedev/taigamobile/viewmodels/utils/TestUtils.kt
+++ app/src/test/kotlin/io/eugenethedev/taigamobile/viewmodels/utils/TestUtils.kt
@@ -1,7 +1,18 @@
 package io.eugenethedev.taigamobile.viewmodels.utils
 
+import io.eugenethedev.taigamobile.domain.entities.TeamMember
+import io.eugenethedev.taigamobile.testdata.User
 import okhttp3.ResponseBody.Companion.toResponseBody
 import retrofit2.HttpException
 import retrofit2.Response
 
 val accessDeniedException = HttpException(Response.error<String>(403, "".toResponseBody(null)))
+
+fun User.toTeamMember(id: Long, name: String, role: String, totalPower: Int) = TeamMember(
+    id = id,
+    avatarUrl = null,
+    name = name,
+    role = role,
+    username = this.username,
+    totalPower = totalPower
+)
