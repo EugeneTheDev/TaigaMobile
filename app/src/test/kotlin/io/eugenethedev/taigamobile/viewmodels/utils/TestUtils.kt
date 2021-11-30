57f34dec5e180c54d70f231de26d19f2ff1bdc60
Eugene Seager <eugene.seager@gmail.com>
2021-11-30 21:12:20 +0300

    Add assertEqualsResult

--- app/src/test/kotlin/io/eugenethedev/taigamobile/viewmodels/utils/TestUtils.kt
+++ app/src/test/kotlin/io/eugenethedev/taigamobile/viewmodels/utils/TestUtils.kt
@@ -2,9 +2,12 @@ package io.eugenethedev.taigamobile.viewmodels.utils
 
 import io.eugenethedev.taigamobile.domain.entities.TeamMember
 import io.eugenethedev.taigamobile.testdata.User
+import io.eugenethedev.taigamobile.ui.utils.Result
 import okhttp3.ResponseBody.Companion.toResponseBody
 import retrofit2.HttpException
 import retrofit2.Response
+import kotlin.test.assertEquals
+
 
 val accessDeniedException = HttpException(Response.error<String>(403, "".toResponseBody(null)))
 
@@ -16,3 +19,9 @@ fun User.toTeamMember(id: Long, name: String, role: String, totalPower: Int) = T
     username = this.username,
     totalPower = totalPower
 )
+
+fun <T> assertResultEquals(expected: Result<T>, actual: Result<T>) {
+    assertEquals(expected::class, actual::class)
+    assertEquals(expected.data, actual.data)
+    assertEquals(expected.message, actual.message)
+}
