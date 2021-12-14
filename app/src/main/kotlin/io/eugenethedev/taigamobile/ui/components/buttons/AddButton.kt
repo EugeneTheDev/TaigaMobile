e801b400f8e62b5a639a715def599f2a0a12d4a5
Eugene Seager <eugene.seager@gmail.com>
2021-12-14 23:34:09 +0300

    Space for between icon and text in AddButton

--- app/src/main/kotlin/io/eugenethedev/taigamobile/ui/components/buttons/AddButton.kt
+++ app/src/main/kotlin/io/eugenethedev/taigamobile/ui/components/buttons/AddButton.kt
@@ -1,10 +1,14 @@
 package io.eugenethedev.taigamobile.ui.components.buttons
 
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import io.eugenethedev.taigamobile.R
 
 /**
@@ -20,6 +24,8 @@ fun AddButton(
             painter = painterResource(R.drawable.ic_add),
             contentDescription = null
         )
+
+        Spacer(Modifier.width(6.dp))
 
         Text(text)
     }
