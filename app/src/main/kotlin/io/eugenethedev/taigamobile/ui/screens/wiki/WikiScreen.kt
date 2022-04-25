89bb216e8219c8cf9ad0439275b8956eed148eea
Михаил Ворожцов <MAVorozhtsov@sberbank.ru>
2022-04-25 20:50:35 +0300

    make route to wiki screen and environment

---
+++ app/src/main/kotlin/io/eugenethedev/taigamobile/ui/screens/wiki/WikiScreen.kt
@@ -0,0 +1,7 @@
+package io.eugenethedev.taigamobile.ui.screens.wiki
+
+/**
+ * 
+ *
+ * @author Имя Фамилия on 25.04.2022
+ */
---
+++ app/src/main/kotlin/io/eugenethedev/taigamobile/ui/screens/wiki/WikiViewModel.kt
@@ -0,0 +1,8 @@
+package io.eugenethedev.taigamobile.ui.screens.wiki
+
+/**
+ * 
+ *
+ * @author Имя Фамилия on 25.04.2022
+ */class WikiViewModel {
+}
